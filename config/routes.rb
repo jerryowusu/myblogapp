397c8dd0460c0e101db99db9722728ac82378287
jerryowusu <jerryowusu6133@gmail.com>
2022-06-02 10:48:10 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root 'users#index'
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
