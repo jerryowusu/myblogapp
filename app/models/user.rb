ec5508584e06c0bc9a5804328aff89b12c6fef97
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-07 10:31:38 -0400

    Create a method that returns the 3 most recent posts for a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    def three_most_recent_posts
+        posts.limit(3).order(created_at: :desc)
+    end
 end
