ca3209faab086bb34f6886e403d867da194a4dab
jerryowusu <jerryowusu6133@gmail.com>
2022-06-07 22:26:02 +0000

    update photo type

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_110101) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "photo"
+    t.string "photo"
     t.text "bio"
     t.integer "posts_counter"
     t.datetime "created_at", null: false
