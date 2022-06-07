85827f06ed730d6d322d1029e6e0128cee94db58
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-07 10:44:18 -0400

    Revert unwanted changes

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
-
-    def three_most_recent_posts
-        posts.limit(3)
-    end
-end
